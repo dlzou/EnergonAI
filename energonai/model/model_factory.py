26a976dafd8c2a11ac2c36a308e4f51b3882bd91
Daniel Zou <zoudl2000@gmail.com>
2023-05-03 14:33:27 -0700

    Add opt-1.3b

--- energonai/model/model_factory.py
+++ energonai/model/model_factory.py
@@ -290,6 +290,21 @@ def opt_125M(**kwargs):
     return create_pipeline_model(**model_kwargs)
 
 
+def opt_1B(**kwargs):
+    model_kwargs = dict(vocab_size=50272,
+                        hidden_size=2048,
+                        depth=24,
+                        max_seq_len=2050,
+                        num_heads=32,
+                        activation=nn.functional.relu,
+                        is_decoder=True,
+                        fused_qkv=False,
+                        model_name="opt",
+                        disable_past_cache=False,
+                        **kwargs)
+    return create_pipeline_model(**model_kwargs)
+
+
 def opt_6B(**kwargs):
     model_kwargs = dict(vocab_size=50272,
                         hidden_size=4096,
